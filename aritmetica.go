ff6316bf2f4166c43c8d49d849e395b7260821c9
Rangel Floripa <rangelflorianopolis@gmail.com>
2022-02-13 19:21:28 -0300

    Aula 13.02.2022

---
+++ aritmetica.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 23
+	var b int = 7
+
+	fmt.Println("a + b =", a + b)
+	fmt.Println("a - b =", a - b)
+	fmt.Println("a * b =", a * b)
+	fmt.Println("a / b =", a / b)
+
+	var c float64 = 23
+	var d float64 = 7
+
+	fmt.Println("c / d =", c / d)
+	fmt.Println("a % b =", a % b)
+}
--- funcoes2.go
+++ funcoes2.go
@@ -2,11 +2,10 @@ package main
 
 import "fmt"
 
-func calcular(a int) (int, int) {
-	var quadrado int = a * a
-	var cubo int = a * a * a
-
-	return quadrado, cubo
+func calcular(a int) (quadrado int, cubo int) {
+	quadrado = a * a
+	cubo = a * a * a
+	return 
 }
 
 func main() {
